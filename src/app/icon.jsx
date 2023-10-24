5587af5050b590d69edf08fd1384224c7f2d38a4
Usmonjon007 <usmonjonhasanov777@gmail.com>
2023-10-24 15:23:45 +0500

    Update icon.jsx

--- src/app/icon.jsx
+++ src/app/icon.jsx
@@ -1,29 +1,31 @@
-import { ImageResponse } from 'next/server'
+import { ImageResponse } from "next/server";
 import Image from "next/image";
 
 // Route segment config
-export const runtime = 'edge'
+export const runtime = "edge";
 
 // Image metadata
 export const size = {
     width: 192,
     height: 192,
-}
-export const contentType = 'image/png'
-
+};
+export const contentType = "image/png";
 
 // Image generation
 export default function Icon() {
     return new ImageResponse(
         (
             // ImageResponse JSX element
-            <img src={`http://localhost:3000/android-chrome-192x192.png`} alt="icon"/>
+            <img
+                src={`http://localhost:3000/android-chrome-192x192.png`}
+                alt="icon"
+            />
         ),
         // ImageResponse options
         {
             // For convenience, we can re-use the exported icons size metadata
-            // config to also set the ImageResponse's width and height.
+            // config to also set the ImageResponse"s width and height.
             ...size,
         }
-    )
+    );
 }
